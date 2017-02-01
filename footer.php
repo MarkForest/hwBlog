c84ed9209b2faa16c45557547ceb3aa48b197ebd
MarkForest <toxerius7@gmail.com>
2017-02-01 05:51:21 +0200

    Структура блога
    Была продумана логика главной и страницы где пост раскрывается...

---
+++ footer.php
@@ -0,0 +1,76 @@
+<!-- Blog Sidebar Widgets Column -->
+<div class="col-md-4">
+
+    <!-- Blog Search Well -->
+    <div class="well">
+        <h4>Поиск в блоге</h4>
+        <div class="input-group">
+            <input type="text" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" type="button">
+                                <span class="glyphicon glyphicon-search"></span>
+                            </button>
+                        </span>
+        </div>
+        <!-- /.input-group -->
+    </div>
+
+    <!-- Blog Categories Well -->
+    <div class="well">
+        <h4>Категории новостей</h4>
+        <div class="row">
+            <div class="col-xs-12">
+                <ul class="list-unstyled">
+                    <li><a href="#">Семья</a>
+                    </li>
+                    <li><a href="#">Работа</a>
+                    </li>
+                    <li><a href="#">Фриланс</a>
+                    </li>
+                    <li><a href="#">Отдых</a>
+                    </li>
+                </ul>
+            </div>
+            <!-- /.col-xs-12 -->
+        </div>
+        <!-- /.row -->
+    </div>
+
+    <!-- Side Widget Well -->
+    <div class="well">
+        <div class="widget">
+            <h4>Кратко о блоге</h4>
+            <p>В этом блоге я рассказываю о себе, своих родных, друзей. Ну и про врагов забывать не будем</p>
+            <img src="images/myfamily.jpg" alt="myfamilly" class="img-responsive">
+        </div>
+    </div>
+</div>
+
+</div>
+<!-- /.row -->
+
+<hr>
+
+<!-- Footer -->
+<footer>
+    <div class="row">
+        <div class="col-lg-12">
+            <p>Copyright &copy; Your Website 2014</p>
+        </div>
+        <!-- /.col-lg-12 -->
+    </div>
+    <!-- /.row -->
+</footer>
+
+</div>
+<!-- /.container -->
+
+<!-- jQuery -->
+<script src="js/jquery.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+$titlePage = "Home";
 try {
     $dsn = "sqlite:blog.sqlite";
     $db = new PDO($dsn);
@@ -24,76 +25,22 @@ try {
 //                        VALUES(1,'$dbdate','$path_img','Племянице купили планшет','$content')";
 //    $db->commit();
 //$db->exec($sql);
-    $sql = "select * from posts,categories where posts.category_id=categories.id order by posts.id desc";
+    if(!isset($_GET['offset'])){
+        $offset = 0;
+    }else{
+        $offset = $_GET['offset'];
+    }
+    $sql = "select * from posts,categories where posts.category_id=categories.id order by posts.id desc limit $offset,4";
+    $sqlCount = "select count(id) from posts";
+    $countPosts="";
+    foreach($db->query($sqlCount) as $row){$countPosts=$row['count(id)'];}
+
 }catch(PDOException $ex){
     $db->rollBack();
     echo "<p style='color:red'>{$ex->getMessage()}</p>";
 }
+include_once "header.php";
 ?>
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>MarkForest's Blog</title>
-
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <link href="css/blog-home.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-
-</head>
-
-<body>
-
-    <!-- Navigation -->
-    <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-        <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="index.php">Блог Марченко Анатолия</a>
-            </div>
-            <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav pull-right">
-                    <li>
-                        <a href="#">Обо мне</a>
-                    </li>
-                    <li>
-                        <a href="#">Сервисы</a>
-                    </li>
-                    <li>
-                        <a href="#">Контакты</a>
-                    </li>
-                    <li>
-                        <a href="#">Регистрация</a>
-                    </li>
-                </ul>
-            </div>
-            <!-- /.navbar-collapse -->
-        </div>
-        <!-- /.container -->
-    </nav>
 
     <!-- Page Content -->
     <div class="container">
@@ -103,11 +50,19 @@ try {
             <!-- Blog Entries Column -->
             <div class="col-md-8">
 
+
                 <h1 class="page-header">
                     Новости
                     <small>Обо всем</small>
                 </h1>
-
+                <ul class="pager">
+                    <li class="previous <?php echo $offset==0?'hide':'show'?>">
+                        <a href="index.php?offset=<?php echo $offset-4?>">&larr; Новые</a>
+                    </li>
+                    <li class="next <?php echo $offset+4>$countPosts?'hide':'show'?>">
+                        <a href="index.php?offset=<?php echo $offset+4?>">Старые &rarr;</a>
+                    </li>
+                </ul>
                 <!--Blog Posts -->
                 <?php foreach($db->query($sql) as $row):?>
                 <h2>
@@ -120,95 +75,25 @@ try {
                 <hr>
                 <img class="img-responsive" src="<?=$row['path_img']?>" alt="">
                 <hr>
-                <p><?=$row['content']?></p>
-                <a class="btn btn-primary" href="post.php">Читать больше<span class="glyphicon glyphicon-chevron-right"></span></a>
+                <p><?php
+                        $str=substr($row['content'],0,500)."...";
+                        echo $str;
+                    ?>
+                </p>
+                <a class="btn btn-primary" href="post.php?id=<?=$row[0]?>">Читать больше<span class="glyphicon glyphicon-chevron-right"></span></a>
                 <hr>
                 <?php endforeach;?>
 
 
                 <!-- Pager -->
                 <ul class="pager">
-                    <li class="previous">
-                        <a href="#">&larr; Старые</a>
+                    <li class="previous <?php echo $offset==0?'hide':'show'?>">
+                        <a href="index.php?offset=<?php echo $offset-4?>">&larr; Новые</a>
                     </li>
-                    <li class="next">
-                        <a href="#">Новые &rarr;</a>
+                    <li class="next <?php echo $offset+4>$countPosts?'hide':'show'?>">
+                        <a href="index.php?offset=<?php echo $offset+4?>">Старые &rarr;</a>
                     </li>
                 </ul>
-
             </div>
 
-            <!-- Blog Sidebar Widgets Column -->
-            <div class="col-md-4">
-
-                <!-- Blog Search Well -->
-                <div class="well">
-                    <h4>Поиск в блоге</h4>
-                    <div class="input-group">
-                        <input type="text" class="form-control">
-                        <span class="input-group-btn">
-                            <button class="btn btn-default" type="button">
-                                <span class="glyphicon glyphicon-search"></span>
-                        </button>
-                        </span>
-                    </div>
-                    <!-- /.input-group -->
-                </div>
-
-                <!-- Blog Categories Well -->
-                <div class="well">
-                    <h4>Категории новостей</h4>
-                    <div class="row">
-                        <div class="col-xs-12">
-                            <ul class="list-unstyled">
-                                <li><a href="#">Семья</a>
-                                </li>
-                                <li><a href="#">Работа</a>
-                                </li>
-                                <li><a href="#">Фриланс</a>
-                                </li>
-                                <li><a href="#">Отдых</a>
-                                </li>
-                            </ul>
-                        </div>
-                        <!-- /.col-xs-12 -->
-                    </div>
-                    <!-- /.row -->
-                </div>
-
-                <!-- Side Widget Well -->
-                <div class="well">
-                    <h4>Side Widget Well</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
-                </div>
-
-            </div>
-
-        </div>
-        <!-- /.row -->
-
-        <hr>
-
-        <!-- Footer -->
-        <footer>
-            <div class="row">
-                <div class="col-lg-12">
-                    <p>Copyright &copy; Your Website 2014</p>
-                </div>
-                <!-- /.col-lg-12 -->
-            </div>
-            <!-- /.row -->
-        </footer>
-
-    </div>
-    <!-- /.container -->
-
-    <!-- jQuery -->
-    <script src="js/jquery.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
-    <script src="js/bootstrap.min.js"></script>
-
-</body>
-
-</html>
+<?php include_once "footer.php";?>
