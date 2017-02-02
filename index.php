a68069f908fe93b5bd4e32d52847119366bfc5d2
MarkForest <toxerius7@gmail.com>
2017-02-02 02:29:37 +0200

    Чистка
    Чистка не использующих переменных, и закоментированого кода

--- index.php
+++ index.php
@@ -5,26 +5,7 @@ try {
     $db = new PDO($dsn);
     $db->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
     $db->beginTransaction();
-    $path_img = "images/9.jpg";
-    $dbdate = date("F d, Y")." at ".date("H:i A");
-    $content = "Давно выяснено, что при оценке дизайна и композиции читаемый
-                текст мешает сосредоточиться. Lorem Ipsum используют потому,
-                 что тот обеспечивает более или менее стандартное заполнение
-                  шаблона, а также реальное распределение букв и пробелов в
-                  абзацах, которое не получается при простой дубликации
-                  \"Здесь ваш текст.. Здесь ваш текст.. Здесь ваш текст..\"
-                   Многие программы электронной вёрстки и редакторы HTML
-                    используют Lorem Ipsum в качестве текста по умолчанию,
-                     так что поиск по ключевым словам \"lorem ipsum\"
-                      сразу показывает, как много веб-страниц всё ещё
-                      дожидаются своего настоящего рождения. За прошедшие
-                       годы текст Lorem Ipsum получил много версий.
-                       Некоторые версии появились по ошибке, некоторые - намеренно
-                       (например, юмористические варианты).";
-//    $sql = "insert into posts(category_id,published_date,path_img,title,content)
-//                        VALUES(1,'$dbdate','$path_img','Племянице купили планшет','$content')";
-//    $db->commit();
-//$db->exec($sql);
+
     if(!isset($_GET['offset'])){
         $offset = 0;
     }else{
@@ -40,6 +21,7 @@ try {
     echo "<p style='color:red'>{$ex->getMessage()}</p>";
 }
 include_once "header.php";
+
 ?>
 
     <!-- Page Content -->
@@ -66,10 +48,10 @@ include_once "header.php";
                 <!--Blog Posts -->
                 <?php foreach($db->query($sql) as $row):?>
                 <h2>
-                    <a href="#"><?=$row['title']?></a>
+                    <a href="post.php?id=<?=$row[0]?>"><?=$row['title']?></a>
                 </h2>
                 <p class="lead">
-                    by <a href="index.php"><?=$row['name']?></a>
+                    Категория:  <a href="index.php"><?=$row['name']?></a>
                 </p>
                 <p><span class="glyphicon glyphicon-time"></span> Опубликовано <?=$row['published_date']?></p>
                 <hr>
