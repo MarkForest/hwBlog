fa793c004fdac14a92bd6b8aebc29692f64b6589
MarkForest <toxerius7@gmail.com>
2017-02-02 04:45:30 +0200

    Корекция адаптивности

--- footer.php
+++ footer.php
@@ -53,13 +53,15 @@
 
 <!-- Footer -->
 <footer>
-    <div class="row">
-        <div class="col-lg-12">
-            <p>Copyright &copy; Your Website 2014</p>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <p>Copyright &copy; Your Website 2014</p>
+            </div>
+            <!-- /.col-lg-12 -->
         </div>
-        <!-- /.col-lg-12 -->
+        <!-- /.row -->
     </div>
-    <!-- /.row -->
 </footer>
 
 </div>
@@ -75,20 +77,23 @@
                 <button type="button" class="close" data-dismiss="modal">&times;</button>
                 <h4 class="modal-title">Добавим пост!</h4>
             </div>
-            <form  method="post" enctype="multipart/form-data">
+            <form  id="formAddPost" method="post" enctype="multipart/form-data">
                 <div class="modal-body">
                     <div class="well">
                         <div class="form-group">
                             <label for="title">Заголовок поста</label>
                             <input class="form-control" type="text" name="title" id="title" placeholder="Введите заголовок...">
+                            <div id="errorTitle"></div>
                         </div>
                         <div class="form-group">
                             <label for="content">Текст поста</label>
                             <textarea rows="4" class="form-control" name="content" id="content" placeholder="Раскройте тему сдесь..."></textarea>
+                            <div id="errorContent"></div>
                         </div>
                         <div class="form-group">
                             <label for="data">Выбор изображения</label>
                             <input type="file" name="data" id="data">
+                            <div id="errorFile"></div>
                         </div>
                         <div class="form-group">
                             <label for="category">Категория поста</label>
